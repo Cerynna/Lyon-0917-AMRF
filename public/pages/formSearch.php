0c7644ff551b8637d7fbc980c699307d04fcccd2
mateevd <mateevd@gmail.com>
2017-10-03 17:54:37 +0200

    changes to search form added advancedd search bar

--- public/pages/formSearch.php
+++ public/pages/formSearch.php
@@ -12,38 +12,42 @@
     </div>
 </nav>
 </div> <!--end of row-->
-
-<!--multiple choice option-->
-<div class="row">
-    <form class="col s12" action="?page=#">
-        <div class="input-field col s12">
-            <select multiple>
-                <option value="" disabled selected>Choisir un thematique</option>
-                <option value="1">Sociale</option>
-                <option value="2">Sante</option>
-                <option value="3">Education</option>
-            </select>
-        </div>
-        <!--auto complete form for departements-->
-        <div class="row">
-            <div class="col s12">
-                <div class="row">
-                    <div class="input-field col s6">
-                        <i class="material-icons prefix">map</i>
-                        <input type="text" id="autocomplete-input" class="autocomplete">
-                        <label for="autocomplete-input">Departement</label>
+<!--Advanced search-->
+<ul class="collapsible" data-collapsible="accordion">
+    <li>
+        <div class="collapsible-header"><i class="material-icons">arrow_drop_down</i>Recherche avancée</div>
+        <div class="collapsible-body">
+            <span>
+                <!--multiple choice option-->
+            <div class="row">
+                <form class="col s12" action="?page=#">
+                    <div class="input-field col s12">
+                        <select multiple>
+                            <option value="" disabled selected>Choisir un thematique</option>
+                            <option value="1">Sociale</option>
+                            <option value="2">Sante</option>
+                            <option value="3">Education</option>
+                        </select>
                     </div>
-                    <div class="input-field col s6">
-                        <input id="codep" type="number" class="validate">
-                        <label for="codep">Code Postal</label>
-                    </div>
-                </div><!--end auto complete-->
-                <div class="row center-align">
-                    <button class="btn waves-effect waves-light" type="submit" name="action">Recherche
-                        <i class="material-icons right">search</i>
-                    </button>
-                </div>
-            </div>
-        </div>
-    </form><!--end of form-->
-</div><!--end of main div-->
+                    <!--auto complete form for departements-->
+                    <div class="row">
+                        <div class="input-field col s6">
+                            <i class="material-icons prefix">map</i>
+                            <input type="text" id="autocomplete-input" class="autocomplete">
+                            <label for="autocomplete-input">Departement</label>
+                        </div>
+                        <div class="input-field col s6">
+                            <input id="codep" type="number" class="validate">
+                            <label for="codep">Code Postal</label>
+                        </div>
+                    </div><!--end auto complete-->
+                </form><!--end of form-->
+            </div><!--end of main row-->
+        </span>
+    </li>
+</ul>
+    <div class="row center-align">
+        <button class="btn waves-effect waves-light" type="submit" name="action">Recherche
+            <i class="material-icons right">search</i>
+        </button>
+    </div>
