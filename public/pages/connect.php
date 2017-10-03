34e67dfa538ab71407a838fcc679e9f78dd7add0
hysteriaskiwo <cerynna@gmail.com>
2017-10-03 18:14:37 +0200

    WIP

---
+++ public/pages/connect.php
@@ -0,0 +1,52 @@
+<h3>Connexion</h3>
+<div class="containerConnect">
+    <div class="card">
+        <div class="formConnect">
+            <h5 class="center-align">Maire</h5>
+            <div class="input-field">
+                <input id="login" type="text" class="validate">
+                <label for="login">n° INSEE</label>
+            </div>
+            <div class="input-field">
+                <input id="mdp" type="password" class="validate">
+                <label for="last_name">Mot de passe</label>
+            </div>
+            <div class="containerConnect">
+                <div class="left-align valign-wrapper">
+                    <a href="#">Créer mon compte</a>
+                </div>
+                <div class="input-field right-align">
+                    <button class="btn" type="submit"
+                            name="action">Connection
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="formConnect">
+            <h5 class="center-align">Partenaire</h5>
+            <div class="input-field">
+                <input id="login" type="text" class="validate">
+                <label for="login">n° SIRET</label>
+            </div>
+            <div class="input-field">
+                <input id="mdp" type="password" class="validate">
+                <label for="last_name">Mot de passe</label>
+            </div>
+            <div class="containerConnect">
+                <div class="left-align valign-wrapper">
+                    <a href="#">Créer mon compte</a>
+                </div>
+                <div class="input-field right-align">
+                    <button class="btn" type="submit"
+                            name="action">Connection
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
