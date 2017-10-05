574f903ffe1e505599c5e06b93212e38929126ff
hysteriaskiwo <cerynna@gmail.com>
2017-10-05 11:54:23 +0200

    Fix syntax

--- dist/structures/head.php
+++ dist/structures/head.php
@@ -1,4 +1,3 @@
-<head>
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css-->
@@ -7,4 +6,3 @@
 
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-</head>
