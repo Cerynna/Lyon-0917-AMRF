482011af6e0746f42acf674520e487fcb6d78220
mateevd <mateevd@gmail.com>
2017-10-09 11:24:32 +0200

    added link from header to home

--- src/structures/header.php
+++ src/structures/header.php
@@ -1,5 +1,5 @@
 <div id="header">
 	<div class="connect hide-on-small-only"><a class="waves-effect waves-light btn modal-trigger teal darken-3" href="#formConnect">Espace Connexion</a>
 	</div>
-	<h1>Le Wiki des Maires</h1>
+	<a href="?page=home"><h1>Le Wiki des Maires</h1></a>
 </div>
