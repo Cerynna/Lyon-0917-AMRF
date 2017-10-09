7cf57f54935c03621c49bae6a5474d06c3d35ae9
mateevd <mateevd@gmail.com>
2017-10-09 10:46:15 +0200

    added composer and src folder

--- index.php
+++ index.php
@@ -1,26 +1,26 @@
 <!doctype html>
 <html lang="fr">
 <head>
-    <?php include "dist/structures/head.php"; ?>
+    <?php include "src/structures/head.php"; ?>
 </head>
-<body>
-<header>
-    <?php include "dist/structures/header.php"; ?>
-</header>
-<?php include "dist/structures/navbar.php"; ?>
-<section>
-    <div class="container content z-depth-4">
-        <div class="row">
-            <div class="col s12">
-                <?php include "dist/structures/rooter.php"; ?>
-            </div>
-        </div>
-    </div>
-</section>
-<footer class="page-footer teal darken-3">
-    <?php include "dist/structures/footer.php"; ?>
-</footer>
-<?php include "dist/structures/connect.php"; ?>
-<?php include "dist/structures/script.php"; ?>
-</body>
+	<body>
+	<header>
+		<?php include "src/structures/header.php"; ?>
+	</header>
+	<?php include "src/structures/navbar.php"; ?>
+	<section>
+		<div class="container content z-depth-4">
+			<div class="row">
+				<div class="col s12">
+					<?php include "src/structures/rooter.php"; ?>
+				</div>
+			</div>
+		</div>
+	</section>
+	<footer class="page-footer teal darken-3">
+		<?php include "src/structures/footer.php"; ?>
+	</footer>
+	<?php include "src/structures/connect.php"; ?>
+	<?php include "src/structures/script.php"; ?>
+	</body>
 </html>
--- src/structures/header.php
+++ src/structures/header.php
@@ -1,8 +1,6 @@
-
-    <div id="header">
+	<div id="header">
         <div class="connect hide-on-small-only"><a class="waves-effect waves-light btn modal-trigger teal darken-3" href="#formConnect">Espace
                 Connexion</a>
         </div>
         <h1>Le Wiki des Maires</h1>
-    </div>
-</header>
+    </div
