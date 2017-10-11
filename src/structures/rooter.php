0655c041c92e1fe05b1af2bab6d10f28c6f3dfa0
Cverine <severinelab@gmail.com>
2017-10-11 14:53:43 +0200

    rename file 4/3

--- src/structures/rooter.php
+++ src/structures/rooter.php
@@ -57,7 +57,7 @@ switch ($page) {
         $container = true;
         break;
     case 'search':
-        $linkPage = "publicRechercheProjet.php";
+        $linkPage = "privateRechercheProjet.php";
         $titlePage = "Recherche";
         $container = true;
         break;
