1b1bc89fbb81c9dfc0009188607c1396b90213c3
Cverine <severinelab@gmail.com>
2017-10-11 17:48:48 +0200

    add changes bootstrap on pages

--- src/pages/privateFavoris.php
+++ src/pages/privateFavoris.php
@@ -1,2 +1,8 @@
 <h2>Gestion des favoris(maires et partenaires</h2>
 
+<div class="card" style="width: 20rem;">
+    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="card-body">
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+</div>
--- src/pages/publicPlanSite.php
+++ src/pages/publicPlanSite.php
@@ -1,28 +1,28 @@
+<h2>Plan du Site</h2>
 <div class="row center-align">
-    <h2 class="center-align">Plan du Site</h2>
-    <div class="col m6">
+    <div class="col-md-6">
 		<ul>
-			<li><a class="planDeSite" href="?page=home">Accueil</a></li>
+			<li><a class="planDeSite" href="?page=Home">Accueil</a></li>
 		</ul>
 		<ul>
-			<li><a class="planDeSite" href="?page=amrf">AMRF</a></li>
+			<li><a class="planDeSite" href="?page=Amrf">AMRF</a></li>
 		</ul>
 		<ul>
-			<li><a class="planDeSite" href="?page=confidentialite">Déclaration de confidentialité</a></li>
+			<li><a class="planDeSite" href="?page=Confidential">Déclaration de confidentialité</a></li>
 		</ul>
 		<ul>
-			<li><a class="planDeSite" href="?page=legales">Conditions d'utilisation</a></li>
+			<li><a class="planDeSite" href="?page=MentionsLegales">Conditions d'utilisation</a></li>
 		</ul>
 	</div>
-	<div class="col m6">
+	<div class="col-md-6">
 		<ul>
-			<li><a class="planDeSite" href="?page=search">Recherche</a></li>
+			<li><a class="planDeSite" href="?page=RechercheProjet">Recherche</a></li>
 		</ul>
 		<ul>
 			<li><a class="planDeSite" href="http://amrf.com" target="_blank">Site AMRF</a></li>
 		</ul>
 		<ul>
-			<li><a class="planDeSite" href="?page=contact">Contact</a></li>
+			<li><a class="planDeSite" href="?page=FormContact">Contact</a></li>
 		</ul>
 		<ul>
 			<li><a class="planDeSite" href="#">Questionnaire de satisfaction</a></li>
