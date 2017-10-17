92fb97d3851888104aa0cb1781da641b159d5bc9
m4rthiz <m4rthiz@gmail.com>
2017-10-17 17:02:43 +0200

    add bootstrap style to PrtListe.php

--- src/pages/privatePartListe.php
+++ src/pages/privatePartListe.php
@@ -8,27 +8,84 @@ $alphabet = [
 <!-- form serach--->
 <section class="row ">
     <h2 class="center-align">Annuaire des partenaires</h2>
-    <div class="row">
-        <p>Trier par lettres</p>
-        <ul class="pagination">
-            <!--<li class="active"><a href="#!">active</a></li>-->
-            <?php foreach ($alphabet as $result): ?>
-                <li class="waves-effect"><a href="#!"><?php echo $result ?></a></li>
-            <?php endforeach; ?>
-        </ul>
-    </div>
-    <div class="row">
-
-            <div class="checkbox">
-                <?php foreach ($secteurs as $result): ?>
-                <label>
-                    <input type="checkbox"><?php echo $result ?>
-                </label>
+    <div class="jumbotron">
+        <div class="row">
+            <p>Trier par lettres</p>
+            <ul class="pagination text-center">
+                <!--<li class="active"><a href="#!">active</a></li>-->
+                <?php foreach ($alphabet as $result): ?>
+                    <li class="waves-effect"><a href="#!"><?php echo $result ?></a></li>
                 <?php endforeach; ?>
-            </div>
-
+            </ul>
+        </div>
+        <div class="form-group row">
+            <p>Trier par thématiques</p>
+            <div class="funkyradio">
+                <div class="col-md-4">
+                    <div class="funkyradio-success">
+                        <input type="checkbox" name="checkbox" id="checkbox1" />
+                        <label for="checkbox1">AMENAGEMENT DU TERRITOIRE</label>
+                    </div>
+                    <div class="funkyradio-success">
+                        <input type="checkbox" name="checkbox" id="checkbox2" />
+                        <label for="checkbox2">CULTURE</label>
+                    </div>
+                    <div class="funkyradio-success">
+                        <input type="checkbox" name="checkbox" id="checkbox3" />
+                        <label for="checkbox3">DEMOCRATIE LOCALE</label>
+                    </div>
+                    <div class="funkyradio-success">
+                        <input type="checkbox" name="checkbox" id="checkbox4" />
+                        <label for="checkbox4">EDUCATION</label>
+                    </div>
+                    <div class="funkyradio-success">
+                        <input type="checkbox" name="checkbox" id="checkbox5" />
+                        <label for="checkbox5">ECONOMIE</label>
+                    </div>
+                </div><!--end of col1-->
+                <div class="col-md-4">
+                    <div class="funkyradio-success">
+                        <input type="checkbox" name="checkbox" id="checkbox6" />
+                        <label for="checkbox6">ENVIRONNEMENT</label>
+                    </div>
+                    <div class="funkyradio-success">
+                        <input type="checkbox" name="checkbox" id="checkbox7" />
+                        <label for="checkbox7">EUA ET ASSAINISSEMENT</label>
+                    </div>
+                    <div class="funkyradio-success">
+                        <input type="checkbox" name="checkbox" id="checkbox8" />
+                        <label for="checkbox8">MOBILITE</label>
+                    </div>
+                    <div class="funkyradio-success">
+                        <input type="checkbox" name="checkbox" id="checkbox9" />
+                        <label for="checkbox9">NUMERIQUE</label>
+                    </div>
+                    <div class="funkyradio-success">
+                        <input type="checkbox" name="checkbox" id="checkbox10" />
+                        <label for="checkbox10">RELATIONS INTERNATIONALES</label>
+                    </div>
+                </div><!--end of col2-->
+                <div class="col-md-4">
+                    <div class="funkyradio-success">
+                        <input type="checkbox" name="checkbox" id="checkbox11" />
+                        <label for="checkbox11">SANTE</label>
+                    </div>
+                    <div class="funkyradio-success">
+                        <input type="checkbox" name="checkbox" id="checkbox12" />
+                        <label for="checkbox12">SOCIAL</label>
+                    </div>
+                    <div class="funkyradio-success">
+                        <input type="checkbox" name="checkbox" id="checkbox13" />
+                        <label for="checkbox13">SERVICES DE PROXIMITE</label>
+                    </div>
+                    <div class="funkyradio-success">
+                        <input type="checkbox" name="checkbox" id="checkbox14" />
+                        <label for="checkbox14">TOURISME</label>
+                    </div>
+                </div><!--end of col3-->
+            </div><!--end of funkyradio-->
+        </div><!--end row/ form-group multi choice-->
     </div>
-
 </section>
 <!-- results -->
 <section class="row">
