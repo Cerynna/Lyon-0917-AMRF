2eeacae3efb91d01e17c9214b44406ddefca9b5d
Cerynna <cerynna@gmail.com>
2017-10-20 11:12:13 +0200

    Update publicHome.php

--- src/pages/publicHome.php
+++ src/pages/publicHome.php
@@ -1,36 +1,38 @@
 <h2>Bienvenue sur le Wiki des Maires</h2>
 <div class="row jumbotron">
-    <div class="col-md-8">
-        <h3>Titre de l'article 1</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum.</p>
+    <div class="col-md-12">
+        <h3>Le Wiki des Maires : qu’est-ce que c’est ? </h3>
+        <p>
+            A destination des adhérents de l’Association des Maires Ruraux de France et de ses partenaires, le Wiki des
+            Maires permet de partager et d’échanger sur des projets mis en place par les communes rurales adhérentes.
+            Plateforme interactive et évolutive, elle illustre le dynamisme de nos communes et l’implication des élus
+            sur leur territoire et pour leurs administrés. Nous ne le dirons jamais assez, l’échelon communal permet
+            l’innovation et la réalisation de projets divers que ce Wiki permet de partager.
+            Alors racontez-nous !
+        </p>
     </div>
-    <div class="col-md-4 right-align"><img class="responsive-img" src="http://lorempixel.com/300/300/nature" alt=""></div>
+    <!--<div class="col-md-4 right-align"><img class="responsive-img" src="http://lorempixel.com/300/300/nature" alt=""></div>-->
 </div>
 <div class="row jumbotron">
-    <div class="col-md-4 left-align"><img class="responsive-img" src="http://lorempixel.com/300/300/nature" alt=""></div>
-    <div class="col-md-8">
-        <h3>Titre de l'article 2</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum.</p>
+    <!--<div class="col-md-4 left-align"><img class="responsive-img" src="http://lorempixel.com/300/300/nature" alt=""></div>-->
+    <div class="col-md-12">
+        <h3>Le Wiki des Maires : comment l’utiliser ? </h3>
+        <p>
+            Vous recherchez un projet mené dans un département spécifique ou une thématique dans laquelle vous souhaitez
+            agir ? Le Wiki vous permet de solliciter l’expertise du réseau via l’expérience des maires et de nos
+            partenaires.
+            Alors inspirez-vous !
+        </p>
     </div>
 </div>
 <div class="row jumbotron">
-    <div class="col-md-8">
-        <h3>Titre de l'article 3</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum.</p>
+    <!--<div class="col-md-4 left-align"><img class="responsive-img" src="http://lorempixel.com/300/300/nature" alt=""></div>-->
+    <div class="col-md-12">
+        <h3>A REFAIRE</h3>
+        <p>
+
+        </p>
     </div>
-    <div class="col-md-4 right-align"><img class="responsive-img" src="http://lorempixel.com/300/300/nature" alt=""></div>
 </div>
 
 <div class="slide-wrap">
