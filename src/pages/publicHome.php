5925d833c61a0139e0a429c66676d98b64b9f7a1
Cverine <severinelab@gmail.com>
2017-10-20 10:55:24 +0200

    add changes on main files and Homepage

--- src/pages/publicHome.php
+++ src/pages/publicHome.php
@@ -32,3 +32,120 @@
     </div>
     <div class="col-md-4 right-align"><img class="responsive-img" src="http://lorempixel.com/300/300/nature" alt=""></div>
 </div>
+
+<div class="slide-wrap">
+    <div class="slide-mask">
+        <ul class="slide-group">
+            <li class="slide"><div class="jumbotron">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img src="http://via.placeholder.com/200x200" alt="">
+                        </div>
+                        <div class="col-md-9">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h3 class="display-3">Projet Application Mobile Court-voiturage</h3>
+                                </div>
+                                <div class="col-md-12 text-left" id="city">
+                                    <span class="label label-success">SAINT JEAN PIED DE PORT</span>
+                                    <span class="label label-success">Pyrénées-Atlantiques</span>
+                                </div>
+                                <div class="col-md-12 text-left" id="theme">
+                                    <span class="label label-default">Mobilité</span>
+                                    <span class="label label-default">Aménagement du territoire</span>
+                                    <span class="label label-default">Aménagement du territoire</span>
+                                </div>
+                                <div class="col-md-12">
+                                    <h3>Résumé du projet</h3>
+                                </div>
+                                <div class="col-md-12">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor                                              incididunt ut labore
+                                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco                                          laboris nisi ut
+                                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate                                        velit esse
+                                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,                                        sunt in
+                                    culpa qui officia deserunt mollit anim id est laborum.
+                                </div>
+                            </div>
+                            <div class="btn btn-success" href="#" role="button">Voir la fiche complète</div>
+                        </div>
+                    </div>
+                </div></li>
+            <li class="slide"><div class="jumbotron">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img src="http://via.placeholder.com/200x200" alt="">
+                        </div>
+                        <div class="col-md-9">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h3 class="display-3">Projet Application Mobile Court-voiturage</h3>
+                                </div>
+                                <div class="col-md-12 text-left" id="city">
+                                    <span class="label label-success">SAINT JEAN PIED DE PORT</span>
+                                    <span class="label label-success">Pyrénées-Atlantiques</span>
+                                </div>
+                                <div class="col-md-12 text-left" id="theme">
+                                    <span class="label label-default">Mobilité</span>
+                                    <span class="label label-default">Aménagement du territoire</span>
+                                    <span class="label label-default">Aménagement du territoire</span>
+                                </div>
+                                <div class="col-md-12">
+                                    <h3>Résumé du projet</h3>
+                                </div>
+                                <div class="col-md-12">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                                    culpa qui officia deserunt mollit anim id est laborum.
+                                </div>
+                            </div>
+                            <div class="btn btn-success" href="#" role="button">Voir la fiche complète</div>
+                        </div>
+                    </div>
+                </div></li>
+            <li class="slide"><div class="jumbotron">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img src="http://via.placeholder.com/200x200" alt="">
+                        </div>
+                        <div class="col-md-9">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h3 class="display-3">Projet Application Mobile Court-voiturage</h3>
+                                </div>
+                                <div class="col-md-12 text-left" id="city">
+                                    <span class="label label-success">SAINT JEAN PIED DE PORT</span>
+                                    <span class="label label-success">Pyrénées-Atlantiques</span>
+                                </div>
+                                <div class="col-md-12 text-left" id="theme">
+                                    <span class="label label-default">Mobilité</span>
+                                    <span class="label label-default">Aménagement du territoire</span>
+                                    <span class="label label-default">Aménagement du territoire</span>
+                                </div>
+                                <div class="col-md-12">
+                                    <h3>Résumé du projet</h3>
+                                </div>
+                                <div class="col-md-12">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                                    culpa qui officia deserunt mollit anim id est laborum.
+                                </div>
+                            </div>
+                            <div class="btn btn-success" href="#" role="button">Voir la fiche complète</div>
+                        </div>
+                    </div>
+                </div></li>
+        </ul>
+    </div>
+
+    <div class="slide-nav">
+        <ul>
+            <li class="bullet"></li>
+            <li class="bullet"></li>
+            <li class="bullet"></li>
+        </ul>
+    </div>
+</div>
