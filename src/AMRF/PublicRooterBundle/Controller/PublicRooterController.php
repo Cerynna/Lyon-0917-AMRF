284cedbb49e11ea911c59cddc16020c6c0662fa2
hysteriaskiwo <cerynna@gmail.com>
2017-10-20 18:21:16 +0200

    TWIG of the night

--- src/AMRF/PublicRooterBundle/Controller/PublicRooterController.php
+++ src/AMRF/PublicRooterBundle/Controller/PublicRooterController.php
@@ -7,31 +7,31 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class PublicRooterController extends Controller
 {
     /**
-     * @Route("/amrf", name="apropos")
+     * @Route("/amrf", name="amrf")
      */
     public function amrfAction()
     {
-        return $this->render('AMRFPublicRooterBundle:Default:index.html.twig');
+        return $this->render('AMRFPublicRooterBundle:Default:amrf.html.twig');
     }
     /**
      * @Route("/contact", name="contact")
      */
     public function contactAction()
     {
-        return $this->render('AMRFPublicRooterBundle:Default:index.html.twig');
+        return $this->render('AMRFPublicRooterBundle:Default:contact.html.twig');
     }
     /**
      * @Route("/confidential", name="confidential")
      */
     public function confidentialAction()
     {
-        return $this->render('AMRFPublicRooterBundle:Default:index.html.twig');
+        return $this->render('AMRFPublicRooterBundle:Default:confidential.html.twig');
     }
     /**
      * @Route("/mentions", name="mentions")
      */
     public function mentionsAction()
     {
-        return $this->render('AMRFPublicRooterBundle:Default:index.html.twig');
+        return $this->render('AMRFPublicRooterBundle:Default:mentions.html.twig');
     }
 }
