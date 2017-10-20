2f599fa0180fe641dab5a268c655017167612c6d
hysteriaskiwo <cerynna@gmail.com>
2017-10-20 18:16:15 +0200

    TWIG of the night

--- oldpages/index.php
+++ oldpages/index.php
@@ -12,13 +12,13 @@
 <div class="body">
 <section>
     <?php echo ($container == true ? "<div class=\"container\">" : "<div class=\"container-fluid\">");  ?>
-    <div class="content z-depth-4">
+    <div class="content">
                 <?php include("src/pages/$linkPage"); ?>
     </div>
     <?php echo ($container == true ? "</div>" : "");  ?>
 </section>
 </div>
-<footer class="page-footer teal darken-3">
+<footer class="page-footer">
     <?php include "src/structures/footer.html.twig"; ?>
 </footer>
 
