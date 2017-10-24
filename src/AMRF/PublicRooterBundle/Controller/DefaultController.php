ab05fb880cd6ef25fe3d204e09d7da0c3417faaf
Jeremy MARTIN <cerynna@gmail.com>
2017-10-24 10:59:04 +0200

    Modif after CodeReview by 2binfree

--- src/AMRF/PublicRooterBundle/Controller/DefaultController.php
+++ src/AMRF/PublicRooterBundle/Controller/DefaultController.php
@@ -3,9 +3,13 @@
 namespace AMRF\PublicRooterBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends Controller
 {
+    /**
+     * @Route("/", name="home")
+     */
     public function indexAction()
     {
         return $this->render('AMRFPublicRooterBundle:public:index.html.twig');
