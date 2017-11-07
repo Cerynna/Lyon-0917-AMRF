d0daa1e26516469791c0bb6da11e6ded301036bb
Jeremy MARTIN <cerynna@gmail.com>
2017-11-07 15:55:33 +0100

    add research in header

--- web/js/main.js
+++ web/js/main.js
@@ -1,18 +1,18 @@
+
+
 $(document).ready(function () {
-
-    $("textarea").attr( "maxlength", 1200 );
-
-
-
 //NavBar
-    $("#navbar").sticky({topSpacing: 140, zIndex: 5});
+    $("#navbar").sticky({topSpacing: 160, zIndex: 6});
     $("#laBar").sticky({topSpacing: 0, zIndex: 4});
-    $("#barRecherche").sticky({topSpacing: 20, zIndex: 4});
+    $("#sticky2").sticky({topSpacing: 20, zIndex: 6,widthFromWrapper: false, center: true});
+    /*$("#sticky3").sticky({topSpacing: 20, zIndex: 6, center: false});*/
 
     //CAROUSSEL
     $('#myCarousel').carousel({
         interval: 3000
     });
+
+    $("textarea").attr( "maxlength", 1200 );x
 
     //Limit Checkbox
     var cb = document.querySelectorAll("[class=check-themat]");
@@ -33,6 +33,7 @@ $(document).ready(function () {
 
 
 });
+
 
 
 function showImg(idimg, img) {
