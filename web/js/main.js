19b8f84845f3d4e4d3fec764462b1383b5c60a48
Jeremy MARTIN <cerynna@gmail.com>
2017-11-08 10:43:58 +0100

    Header + SearchBar

--- web/js/main.js
+++ web/js/main.js
@@ -1,18 +1,31 @@
+$(window).on('scroll', function () {
+    var scrollTop = $(window).scrollTop();
+    if (scrollTop > 20) {
+        $('#header').stop().animate({height: "170"},5);
 
+        $('#titleHeader').stop().fadeOut(50);
+    }
+    else {
+        $('#header').stop().animate({height: "400"},5);
+
+        $('#titleHeader').stop().fadeIn(50);
+    }
+});
 
 $(document).ready(function () {
+
+    $("textarea").attr( "maxlength", 1200 );
+
+
 //NavBar
-    $("#navbar").sticky({topSpacing: 158, zIndex: 6});
-    /*$("#laBar").sticky({topSpacing: 0, zIndex: 4});*/
-    $("#sticky2").sticky({topSpacing: 20, zIndex: 6,widthFromWrapper: true, center: true});
-    /*$("#sticky3").sticky({topSpacing: 20, zIndex: 6, center: false});*/
+    $("#navbar").sticky({topSpacing: 150, zIndex: 6});
+    $("#search").sticky({topSpacing: 0, zIndex: 6,getWidthFrom: "500px", center: true});
+
 
     //CAROUSSEL
     $('#myCarousel').carousel({
         interval: 3000
     });
-
-    $("textarea").attr( "maxlength", 1200 );x
 
     //Limit Checkbox
     var cb = document.querySelectorAll("[class=check-themat]");
