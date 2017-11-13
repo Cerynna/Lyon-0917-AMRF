e3316ef47b12c2687e7e2725d0fcd794ef245782
mateevd <mateevd@gmail.com>
2017-11-13 17:10:36 +0100

    add swiper for maire profil

--- web/js/main.js
+++ web/js/main.js
@@ -18,6 +18,28 @@ $(document).ready(function () {
 
     $("textarea").attr("maxlength", 1200);
 
+    //Swiper
+    var swiper = new Swiper ('.swiper-container', {
+        // Optional parameters
+        loop: true,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination'
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        },
+
+        // And if we need scrollbar
+        scrollbar:{
+            el: '.swiper-scrollbar'
+        }
+    });
+
 //NavBar
     /* $("#navbar").sticky({topSpacing: 150, zIndex: 6});*/
     /*$("#search").sticky({topSpacing: 0, zIndex: 6,getWidthFrom: "500px", center: true});*/
