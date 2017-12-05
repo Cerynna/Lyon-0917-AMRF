4bef5854247cdcf9ebbc17562be8f387e62426cd
mateevd <mateevd@gmail.com>
2017-12-05 15:31:38 +0100

    add pagination + sort fields to admin/users

--- src/AppBundle/Controller/AdminUserController.php
+++ src/AppBundle/Controller/AdminUserController.php
@@ -18,143 +18,158 @@ class AdminUserController extends Controller
 {
 
 
-    /**
-     * Lists all user entities.
-     *
-     * @Route("/", name="admin_user_index")
-     * @Method("GET")
-     */
-    public function indexAction()
-    {
-        $em = $this->getDoctrine()->getManager();
+	/**
+	 * Lists all user entities.
+	 *
+	 * @Route("/", name="admin_user_index")
+	 * @Method("GET")
+	 */
+	public function indexAction(Request $request)
+	{
+		$em = $this->getDoctrine()->getManager();
 
-        $users = $em->getRepository('AppBundle:User')->findAll();
+		/*        $users = $em->getRepository('AppBundle:User')->findAll();*/
 
-        return $this->render('user/index.html.twig', array(
-            'users' => $users,
-        ));
-    }
+		$dql = "SELECT u FROM AppBundle:User u";
+		$query = $em->createQuery($dql);
 
-    /**
-     * Creates a new user entity.
-     *
-     * @Route("/new", name="admin_user_new")
-     * @Method({"GET", "POST"})
-     */
-    public function newAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
-    {
-        $user = new User();
-        $form = $this->createForm('AppBundle\Form\UserType', $user);
-        $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
+		/**
+		 * @var $paginator \Knp\Component\Pager\Paginator
+		 */
+		$paginator = $this->get('knp_paginator');
+		$result = $paginator->paginate(
+			$query,
+			$request->query->getInt('page', 1),
+			$request->query->getInt('limit', 10)
+		);
 
-            $password = $passwordEncoder->encodePassword($user, $user->getPassword());
-            $user->setPassword($password);
+		dump(get_class($paginator));
 
-            if ($user->getRole() === User::USER_ROLE_MAYOR){
-                $user->setPartner(null);
-            }
-            if ($user->getRole() === User::USER_ROLE_PARTNER){
-                $user->setMayor(null);
-            }
-            if ($user->getRole() === User::USER_ROLE_ADMIN){
-                $user->setMayor(null);
-                $user->setPartner(null);
-            }
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($user);
-            $em->flush();
+		return $this->render('user/index.html.twig', array(
+			'users' => $result,
+		));
+	}
 
-            return $this->redirectToRoute('admin_user_show', array('id' => $user->getId()));
-        }
-        return $this->render('user/new.html.twig', array(
-            'user' => $user,
-            'form' => $form->createView(),
-        ));
-    }
+	/**
+	 * Creates a new user entity.
+	 *
+	 * @Route("/new", name="admin_user_new")
+	 * @Method({"GET", "POST"})
+	 */
+	public function newAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+	{
+		$user = new User();
+		$form = $this->createForm('AppBundle\Form\UserType', $user);
+		$form->handleRequest($request);
 
-    /**
-     * Finds and displays a user entity.
-     *
-     * @Route("/{id}", name="admin_user_show")
-     * @Method("GET")
-     */
-    public function showAction(User $user)
-    {
-        $deleteForm = $this->createDeleteForm($user);
+		if ($form->isSubmitted() && $form->isValid()) {
 
-        return $this->render('user/show.html.twig', array(
-            'user' => $user,
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
+			$password = $passwordEncoder->encodePassword($user, $user->getPassword());
+			$user->setPassword($password);
 
-    /**
-     * Displays a form to edit an existing user entity.
-     *
-     * @Route("/{id}/edit", name="admin_user_edit")
-     * @Method({"GET", "POST"})
-     */
-    public function editAction(Request $request, User $user, UserPasswordEncoderInterface $passwordEncoder)
-    {
-        $deleteForm = $this->createDeleteForm($user);
-        $editForm = $this->createForm('AppBundle\Form\UserType', $user);
-        $editForm->handleRequest($request);
+			if ($user->getRole() === User::USER_ROLE_MAYOR) {
+				$user->setPartner(null);
+			}
+			if ($user->getRole() === User::USER_ROLE_PARTNER) {
+				$user->setMayor(null);
+			}
+			if ($user->getRole() === User::USER_ROLE_ADMIN) {
+				$user->setMayor(null);
+				$user->setPartner(null);
+			}
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($user);
+			$em->flush();
 
-        if ($editForm->isSubmitted() && $editForm->isValid()) {
+			return $this->redirectToRoute('admin_user_show', array('id' => $user->getId()));
+		}
+		return $this->render('user/new.html.twig', array(
+			'user' => $user,
+			'form' => $form->createView(),
+		));
+	}
 
-            $password = $passwordEncoder->encodePassword($user, $user->getPassword());
-            $user->setPassword($password);
+	/**
+	 * Finds and displays a user entity.
+	 *
+	 * @Route("/{id}", name="admin_user_show")
+	 * @Method("GET")
+	 */
+	public function showAction(User $user)
+	{
+		$deleteForm = $this->createDeleteForm($user);
 
-            $this->getDoctrine()->getManager()->flush();
-            $this->addFlash(
-                'notice',
-                'Le user n°'. $user->getLogin() . ' a été modifié.'
-            );
-            return $this->redirectToRoute('admin_user_edit', array('id' => $user->getId()));
-        }
+		return $this->render('user/show.html.twig', array(
+			'user' => $user,
+			'delete_form' => $deleteForm->createView(),
+		));
+	}
 
-        return $this->render('user/edit.html.twig', array(
-            'user' => $user,
-            'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
+	/**
+	 * Displays a form to edit an existing user entity.
+	 *
+	 * @Route("/{id}/edit", name="admin_user_edit")
+	 * @Method({"GET", "POST"})
+	 */
+	public function editAction(Request $request, User $user, UserPasswordEncoderInterface $passwordEncoder)
+	{
+		$deleteForm = $this->createDeleteForm($user);
+		$editForm = $this->createForm('AppBundle\Form\UserType', $user);
+		$editForm->handleRequest($request);
 
-    /**
-     * Deletes a user entity.
-     *
-     * @Route("/{id}", name="admin_user_delete")
-     * @Method("DELETE")
-     */
-    public function deleteAction(Request $request, User $user)
-    {
-        $form = $this->createDeleteForm($user);
-        $form->handleRequest($request);
+		if ($editForm->isSubmitted() && $editForm->isValid()) {
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($user);
-            $em->flush();
-        }
+			$password = $passwordEncoder->encodePassword($user, $user->getPassword());
+			$user->setPassword($password);
 
-        return $this->redirectToRoute('admin_user_index');
-    }
+			$this->getDoctrine()->getManager()->flush();
+			$this->addFlash(
+				'notice',
+				'Le user n°' . $user->getLogin() . ' a été modifié.'
+			);
+			return $this->redirectToRoute('admin_user_edit', array('id' => $user->getId()));
+		}
 
-    /**
-     * Creates a form to delete a user entity.
-     *
-     * @param User $user The user entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm(User $user)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('admin_user_delete', array('id' => $user->getId())))
-            ->setMethod('DELETE')
-            ->getForm()
-        ;
-    }
+		return $this->render('user/edit.html.twig', array(
+			'user' => $user,
+			'edit_form' => $editForm->createView(),
+			'delete_form' => $deleteForm->createView(),
+		));
+	}
+
+	/**
+	 * Deletes a user entity.
+	 *
+	 * @Route("/{id}", name="admin_user_delete")
+	 * @Method("DELETE")
+	 */
+	public function deleteAction(Request $request, User $user)
+	{
+		$form = $this->createDeleteForm($user);
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted() && $form->isValid()) {
+			$em = $this->getDoctrine()->getManager();
+			$em->remove($user);
+			$em->flush();
+		}
+
+		return $this->redirectToRoute('admin_user_index');
+	}
+
+	/**
+	 * Creates a form to delete a user entity.
+	 *
+	 * @param User $user The user entity
+	 *
+	 * @return \Symfony\Component\Form\Form The form
+	 */
+	private function createDeleteForm(User $user)
+	{
+		return $this->createFormBuilder()
+			->setAction($this->generateUrl('admin_user_delete', array('id' => $user->getId())))
+			->setMethod('DELETE')
+			->getForm();
+	}
 }
