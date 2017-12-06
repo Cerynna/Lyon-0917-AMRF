cca0b91697ccad833651dc5791f4778d6e5626d4
Jeremy MARTIN <cerynna@gmail.com>
2017-12-06 17:30:25 +0100

    :poop: Fiche Projet Maire :poop:

--- src/AppBundle/Controller/MayorController.php
+++ src/AppBundle/Controller/MayorController.php
@@ -14,12 +14,15 @@ use AppBundle\Entity\Company;
 use AppBundle\Entity\Project;
 
 use AppBundle\Entity\TitleProject;
+use AppBundle\Form\SubmitToAdmin;
 use AppBundle\Service\SlugService;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
+const MAX_TAB = 5;
 
 /**
  * @Route("mayor/")
@@ -81,6 +84,7 @@ class MayorController extends Controller
         $user = $this->get('security.token_storage')->getToken()->getUser();
         $MayorConnect = $user->getMayor();
 
+
         if ($form->isSubmitted() && $form->isValid()) {
 
             $project = new Project();
@@ -90,13 +94,13 @@ class MayorController extends Controller
             $project->setUpdateDate(new \DateTime('now'));
             $project->setProjectDate(new \DateTime('now'));
             $project->setSlug($slugService->slug($projectTitle->getTitle()));
-
+            $project->setStatus(Project::STATUS_DRAFT);
 
             $em = $this->getDoctrine()->getManager();
 
             $em->persist($project);
             $em->flush();
-            return $this->redirectToRoute('mayor_project_edit', array('id' => $project->getId()));
+            return $this->redirectToRoute('mayor_project_edit', array('slug' => $project->getSlug()));
         }
 
 
@@ -107,17 +111,26 @@ class MayorController extends Controller
     }
 
     /**
-     * @Route("project/edit/{id}", name="mayor_project_edit")
+     * @Route("project/edit/{slug}/{page}", name="mayor_project_edit", defaults={"page": "1"},)
      * @Method({"GET", "POST"})
      */
     public function mayorProjectEditAction(Request $request, Project $project, SlugService $slugService)
     {
+
         $user = $this->get('security.token_storage')->getToken()->getUser();
-        $idMayorConnect = $user->getMayor()->getId();
-        $idMayorProject = $project->getMayor()->getId();
+        $idMayorConnect = $user->getMayor();
+        $idMayorProject = $project->getMayor();
+
+        $uri = $request->getPathInfo();
+        $uriExplode = explode('/', $uri);
+        $page = array_pop($uriExplode);
+        if (!is_numeric($page)) {
+            $page = 1;
+        }
 
 
-        if ($idMayorConnect === $idMayorProject) {
+        if ($idMayorConnect->getId() === $idMayorProject->getId()) {
+            $em = $this->getDoctrine()->getManager();
             $form = $this->createForm('AppBundle\Form\ProjectType', $project);
             $form->remove('images');
             $form->remove('file');
@@ -126,25 +139,61 @@ class MayorController extends Controller
             $form->handleRequest($request);
 
 
+            $submitToAdmin = new SubmitToAdmin();
+            $formSubmitToAdmin = $this->createForm('AppBundle\Form\SubmitToAdmin', $submitToAdmin);
+            $formSubmitToAdmin->handleRequest($request);
 
+            if ($formSubmitToAdmin->isSubmitted() && $formSubmitToAdmin->isValid()) {
+                $project->setStatus(Project::STATUS_WAITING);
+                $em->persist($project);
+                $em->flush();
+                return $this->redirectToRoute('mayor_project_edit', [
+                    'slug' => $project->getSlug(),
+                ]);
+            }
             if ($form->isSubmitted() && $form->isValid()) {
-                $em = $this->getDoctrine()->getManager();
-                $project->setSlug($slugService->slug($projectTitle->getTitle()));
+
+                $project->setSlug($slugService->slug($project->getTitle()));
+                $project->setStatus(Project::STATUS_DRAFT);
+                $project->setMayor($idMayorConnect);
                 $em->persist($project);
                 $em->flush();
 
-                return $this->redirectToRoute('mayor_project_edit', array('id' => $project->getId()));
+                if(isset($_POST['page'])){
+                    switch ($_POST['page']){
+                        case 'next':
+                                $page = $page + 1;
+                            break;
+                        case 'back':
+                                $page = $page - 1;
+                            break;
+                    }
+                }
+                if ($page > 5) {
+                    $page = 5;
+                }
+                if ($page < 1 ){
+                    $page =1;
+                }
+
+               /* return new Response("FORM SEND ". serialize($form->getData()) . " - " . $_POST['page'] . " - " . $page  );*/
+                return $this->redirectToRoute('mayor_project_edit', [
+                    'slug' => $project->getSlug(),
+                    'page' => $page,
+
+                ]);
             }
 
             return $this->render('private/maires/projectEdit.html.twig', array(
+                'slug' => $project->getSlug(),
                 'project' => $project,
                 'form' => $form->createView(),
+                'form_toAdmin' => $formSubmitToAdmin->createView(),
             ));
         }
         else {
             return $this->redirectToRoute('mayor_index');
         }
-
     }
 
     /**
