568117f208855b20e9a968aaa8271e1cfbe2cd3f
mateevd <mateevd@gmail.com>
2018-01-31 11:11:27 +0100

    pdf download

--- src/AppBundle/Controller/pdfController.php
+++ src/AppBundle/Controller/pdfController.php
@@ -8,7 +8,6 @@
 
 namespace AppBundle\Controller;
 
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,41 +15,43 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class pdfController extends Controller
 {
+	const name = '0123456789abcdefghijklmnopqrstuvwxyz';
 
 	/**
 	 * Export to PDF
 	 *
-	 * @Route("/pdf", name="htmlTOpdf")
+	 * @Route("/pdf/{idProject}", name="htmlTOpdf")
 	 */
-	public function indexAction(Request $request)
+	public function indexAction(Request $request, $idProject)
 	{
 		$snappy = $this->get('knp_snappy.pdf');
 
 		$project = $this->getDoctrine()->getRepository('AppBundle:Project')
-			->projectById(11);
-		/*$appPath = $this->container->getParameter('kernel.project_dir');
-		$webPath = realpath($appPath . '/../web/img/');*/
+			->projectById($idProject);
 
 		$html = $this->renderView('components/pdf.html.twig', array(
 			'project' => $project[0],
 			'base_dir' => $this->get('kernel')->getRootDir() . '/../web' . $request->getBasePath()
 		));
 
-		$filename = 'myFirstSnappyPDF';
+		$filename = str_shuffle(self::name);
 
 		return new Response(
 			$snappy->getOutputFromHtml($html, array(
-				'lowquality' => false,
-				'print-media-type' => true,
-				'images' => true,
-				'dpi' => 1300,
-				'image-dpi' => 1300,
-				'encoding' => 'utf-8'
+				'page-size' 		=> 'A4',
+				'lowquality' 		=> false,
+				'print-media-type'  => true,
+				'images' 			=> true,
+				'footer-center' 	=> 'Page [page] of [toPage]',
+				'dpi' 				=> 500,
+				'image-dpi' 		=> 500,
+				'encoding' 			=> 'utf-8',
+
 			)),
 			200,
 			array(
 				'Content-Type'          => 'application/pdf',
-				'Content-Disposition'   => 'inline; filename="'.$filename.'.pdf"',
+				'Content-Disposition' 	=>  'application; filename="'.$filename.'.pdf"',
 			)
 		);
 	}
